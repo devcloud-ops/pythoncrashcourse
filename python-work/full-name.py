7b1033d3614a02e883da03d1eae70bb8f1bf8d36
DevCloudOps <devcloud.ops@outlook.com>
2021-07-16 14:56:28 +1000

    testing some functions

---
+++ python-work/full-name.py
@@ -0,0 +1,11 @@
+first_name = 'ada'
+last_name = 'lovelace'
+# Notice the f before the quotation marks. This f lets Python interpreter format the string to replace variable values.
+full_name = f'{first_name} {last_name}'
+print(full_name)
+print(full_name.title())
+print(full_name.capitalize())
+print(full_name.casefold())
+#print(full_name.center())
+print(full_name.count('e'))
+print(full_name.find('a'))
