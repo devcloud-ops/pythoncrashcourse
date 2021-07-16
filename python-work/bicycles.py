f44f50c30ca90f6b0a8c4b5bdae6b978ad1f84c9
DevCloudOps <devcloud.ops@outlook.com>
2021-07-16 17:23:25 +1000

    chapter3-lists

---
+++ python-work/bicycles.py
@@ -0,0 +1,14 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[2])
+print(bicycles[3])
+print(bicycles[2], bicycles[3])
+
+# applying string functions to list elements
+print(bicycles[0].title())
+print(bicycles[1].capitalize())
+print(bicycles[2].count('e'))
+
