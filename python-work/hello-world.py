8cc67e95c92277fdbcd439552d43f02dc4e2428b
DevCloudOps <devcloud.ops@outlook.com>
2021-07-16 13:31:55 +1000

    added second message variable

--- python-work/hello-world.py
+++ python-work/hello-world.py
@@ -1,3 +1,6 @@
 # This program prints hello world to the console.
 message="This is a comment inside a variable"
 print(message);
+
+message="This is a second message with the same variable name"
+print(message)
