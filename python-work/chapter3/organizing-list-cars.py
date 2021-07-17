05d855e4ba8c03a11427c910020379fa4f3ac2dd
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 15:39:22 +1000

    sorting-temporarily-using-sorted-function

--- python-work/chapter3/organizing-list-cars.py
+++ python-work/chapter3/organizing-list-cars.py
@@ -27,3 +27,26 @@ print(f'Before sorting the alphabets in reverse alphabetical order: {alphabets}'
 
 alphabets.sort(reverse=True)
 print(f'After sorting the alphabets in reverse alphabetical order: {alphabets}\n')
+
+############################################################################################
+
+print ('\n\n--------------------------------------------------------------------------\n\n')
+
+############################################################################################
+
+#Using sorted() method on lists to sort them temporarily in alphabetical order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(f'\nBefore sorting the cars in temporarily in alphabetical order: {cars}')
+
+sorted(cars) #The changes to the cars list is valid and applicable till this line only
+print(f'During sorting the cars temporarily in alphabetical order: {sorted(cars)}')
+print(f'After sorting the cars in alphabetical order: {cars}\n')
+
+
+alphabets=['c','e','d','a','z','b','y']
+print(f'Before sorting the alphabets in alphabetical order: {alphabets}')
+
+sorted(alphabets)#The changes to the alphabets list is valid and applicable till this line only
+print(f'During sorting the alphabets temporarily in alphabetical order: {sorted(alphabets)}')
+print(f'After sorting the alphabets in alphabetical order: {alphabets}\n')
+
