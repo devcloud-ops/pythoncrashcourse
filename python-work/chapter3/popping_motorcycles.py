310e9abdd42ec41d979ab2f6fba0deab6d485180
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 13:50:17 +1000

    popping-specific-element-from-list

--- python-work/chapter3/popping_motorcycles.py
+++ python-work/chapter3/popping_motorcycles.py
@@ -11,3 +11,21 @@ popped_motorcycle2 = motorcycles.pop()
 print(f"After popping the motorcycles, the initial object becomes: {motorcycles}\n")
 print(f"Popped motorcycle during first occurence: {popped_motorcycle}")
 print(f"Popped motorcycle during second occurence: {popped_motorcycle2}")
+
+
+#############################################################################
+
+
+#popping an element from specific position/index of the list
+
+#Add 3 motorcycle elements in a list of motorcycles
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+#print the motorcycles list to console
+print(f"\nInitial number of motorcycles list: {motorcycles} \n")
+
+#Pop second element in the list
+second_motorcycle = motorcycles.pop(1)
+
+print(f"After popping the motorcycles, the initial object becomes: {motorcycles}\n")
+print(f"Popped motorcycle: {second_motorcycle}")
