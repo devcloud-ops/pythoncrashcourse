f3363d3cef62f7e037f3d5444593c427808594ea
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 14:08:16 +1000

    guest-list-3-4

---
+++ python-work/chapter3/guest-list-3-4.py
@@ -0,0 +1,16 @@
+# If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+
+guest_list=[]
+guest_list.append ('alex')
+guest_list.append ('paul')
+guest_list.append ('john')
+
+print(f'\nI have invitied {len(guest_list)} guests for dinner including {guest_list}\n')
+
+print(f'Hey {guest_list[0].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+print(f'Hey {guest_list[1].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+print(f'Hey {guest_list[2].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+
