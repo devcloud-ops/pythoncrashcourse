df5b3d5279230c90ce8fe95224c118e89e550266
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 15:46:32 +1000

    using-sorted-reverse-alphabetical-true

--- python-work/chapter3/organizing-list-cars.py
+++ python-work/chapter3/organizing-list-cars.py
@@ -50,3 +50,26 @@ sorted(alphabets)#The changes to the alphabets list is valid and applicable till
 print(f'During sorting the alphabets temporarily in alphabetical order: {sorted(alphabets)}')
 print(f'After sorting the alphabets in alphabetical order: {alphabets}\n')
 
+############################################################################################
+
+print ('\n\n--------------------------------------------------------------------------\n\n')
+
+############################################################################################
+
+#Using sorted() method on lists to sort them temporarily in reverse alphabetical order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(f'\nBefore sorting the cars temporarily in reverse alphabetical order: {cars}')
+
+sorted(cars, reverse=True) #The changes to the cars list is valid and applicable till this line only
+print(f'During sorting the cars temporarily in reverse alphabetical order: {sorted(cars, reverse=True)}')
+print(f'After sorting the cars in reverse alphabetical order: {cars}\n')
+
+
+alphabets=['c','e','d','a','z','b','y']
+print(f'Before sorting the alphabets in reverse alphabetical order: {alphabets}')
+
+sorted(alphabets, reverse=True)#The changes to the alphabets list is valid and applicable till this line only
+print(f'During sorting the alphabets temporarily in reverse alphabetical order: {sorted(alphabets, reverse=True)}')
+print(f'After sorting the alphabets in reverse alphabetical order: {alphabets}\n')
+
+
