f2336da4c883525808b54f5ee918cf4dcb8fc302
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 18:53:53 +1000

    number-statistics

---
+++ python-work/chapter4/statistics.py
@@ -0,0 +1,14 @@
+# Number Statistics
+
+digits = list(range(0,10))
+print(digits)
+# Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(min(digits))
+# Output: 0
+
+print(max(digits))
+# Output: 9
+
+print(sum(digits))
+# Output: 45
