0ef5cb1879234e98f65fbaef7575598d045229ec
DevCloudOps <devcloud.ops@outlook.com>
2021-07-17 14:09:37 +1000

    output-added-in-comments

--- python-work/chapter3/guest-list-3-4.py
+++ python-work/chapter3/guest-list-3-4.py
@@ -9,8 +9,13 @@ guest_list.append ('paul')
 guest_list.append ('john')
 
 print(f'\nI have invitied {len(guest_list)} guests for dinner including {guest_list}\n')
+#output: I have invitied 3 guests for dinner including ['alex', 'paul', 'john']
 
 print(f'Hey {guest_list[0].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
-print(f'Hey {guest_list[1].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
-print(f'Hey {guest_list[2].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+#output: Hey Alex, you are cordially invited to my place for dinner on 3-3-3033.
 
+print(f'Hey {guest_list[1].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+#output: Hey Paul, you are cordially invited to my place for dinner on 3-3-3033.
+
+print(f'Hey {guest_list[2].title()}, you are cordially invited to my place for dinner on 3-3-3033.\n')
+#output: Hey John, you are cordially invited to my place for dinner on 3-3-3033.
