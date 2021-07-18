4d132c95cc43c7a46a3b0a1e5c1983b8de6553c5
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 12:16:02 +1000

    amusement_park_approach2

--- python-work/chapter5/amusement_park.py
+++ python-work/chapter5/amusement_park.py
@@ -1,12 +1,28 @@
 # Prints result of admission based on age
 
-#age = 3
-#age = 16
-age = 30
+# age = 3
+# age = 16
+# age = 30
+age = 67
 
+# Approach 1
+# if age < 4:
+#     print('\nYour admission cost is $0.')
+# elif age < 18:
+#     print('\nYour admission cost is $25.')
+# else:
+#     print('\nYour admission cost is $40.')
+
+
+# Approach 2: More appropriate
 if age < 4:
-    print('\nYour admission cost is $0.')
+    price = 0 # babies are exempted
 elif age < 18:
-    print('\nYour admission cost is $25.')
+    price = 25 # children and teen
+elif age < 65:
+    price = 40 # adult
 else:
-    print('\nYour admission cost is $40.')
+    price = 20 # seniors discount
+    print('\nSeniors discount applied!')
+
+print(f'\nYour admission cost is ${price}.')
