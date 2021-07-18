5c723e5e3c6a4623c3a04a6e3d0606555b084ab8
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 14:06:55 +1000

    apply-if-else-statements-to-list-items

--- python-work/chapter5/toppings.py
+++ python-work/chapter5/toppings.py
@@ -1,8 +1,18 @@
-requested_topping = 'mushrooms'
+#requested_topping = 'mushrooms'
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
-if 'mushrooms' in requested_topping:
-    print('great job, proceed with the order!')
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
+
+# if 'mushrooms' in requested_topping:
+#     print('great job, proceed with the order!')
+    
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("\nSorry, we are out of green peppers right now.")
+    else:
+        print(f"\nAdding {requested_topping}.")
+
+print("\nFinished making your pizza!")
 
