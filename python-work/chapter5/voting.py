f11f8257051f421dcf9999c0d5d677fbe14a214f
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 11:59:01 +1000

    else-statement-in-action

--- python-work/chapter5/voting.py
+++ python-work/chapter5/voting.py
@@ -1,4 +1,8 @@
-age = 19
+# age = 19
+age = 17
 if (age >= 18):
     print('\nYou are old enough to Vote!')
     print('Have you registered to Vote yet?')
+else:
+    print('\nSorry, you are too young to Vote!')
+    print('Please register to Vote as soon as you turn 18!')
