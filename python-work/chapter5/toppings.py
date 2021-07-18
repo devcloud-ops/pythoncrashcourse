ecaf9b3f3cb003c499c7bf9aa470f62ce16e55ef
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 11:07:07 +1000

    checking-for-a-value-in-a-list-using-if-statements

--- python-work/chapter5/toppings.py
+++ python-work/chapter5/toppings.py
@@ -3,3 +3,6 @@ requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
 
+if 'mushrooms' in requested_topping:
+    print('great job, proceed with the order!')
+
