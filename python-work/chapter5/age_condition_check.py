fc8902997dded2b8d9e8e19157228d15886a7011
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 11:01:42 +1000

    age-condition-check

---
+++ python-work/chapter5/age_condition_check.py
@@ -0,0 +1,12 @@
+age_faisal=40
+age_abdullah=12
+
+corret_statement=' '.join(("\nthat is correct, Faisal's age is more than 30",
+                           "and Abdullah's age is less than 40"))
+
+if (age_faisal >= 30) and (age_abdullah<=40):
+    print(corret_statement)
+    print('\nThat\'s correct answer!')
+
+if (age_faisal >= 30) and (age_abdullah<=11):
+    print('that is not the correct answer!')
