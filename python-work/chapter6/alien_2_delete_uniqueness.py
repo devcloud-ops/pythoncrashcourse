1123ac2c904738b49bafe883319c25f838a5ab23
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 18:42:45 +1000

    file-names-updated

---
+++ python-work/chapter6/alien_2_delete_uniqueness.py
@@ -0,0 +1,7 @@
+# Unique key value pairs only, no duplicate values allowed in dic, unlike lists
+alien_1 = {'color': 'green', 'points': 5, 'color': 'red'}
+print(alien_1)
+
+# to delete a certain key-value pair, simply, specify the dictionary with key:
+del alien_1['points']
+print(alien_1)
