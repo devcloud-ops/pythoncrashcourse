b532c98d2b68eac3b448da5469b49a952e203463
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 15:08:01 +1000

    checking_username_availability_5_10

--- python-work/chapter5/checking-usernames-5-10.py
+++ python-work/chapter5/checking-usernames-5-10.py
@@ -10,3 +10,31 @@
 # d) Make sure your comparison is case insensitive. If 'John' has been used,
 # 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of
 # current_users containing the lowercase versions of all existing users.)
+
+current_users = ['mAnsoor', 'faisaL', 'Salman', 'fahaD', 'Nauman']
+
+# Creating a new list that stores case insensitive "current_users" list.
+current_users_case_insensitive = []
+for user in current_users:
+    current_users_case_insensitive.append(user.lower())
+
+# verify that the new list is indeed case insensitive
+print(current_users_case_insensitive)
+
+new_users = ['samir', 'umer', 'faisal', 'yasir', 'badar']
+# new_users = []
+
+# ensure that the current_users or new_users list aren't empty:
+if new_users:
+    for new_user in new_users:
+        if new_user.lower() in current_users_case_insensitive:
+            username_unavailable = ' '.join((f"Your entered username:{new_user}",
+                                        "is already in use. Please provide",
+                                        "a new username!"))
+            print(username_unavailable)
+        else:
+            username_available = ' '.join(("Congratulations! this username:",
+                                        f"{new_user} is available for use!"))
+            print(username_available)
+else:
+    print('\nWe need to find some users!')
--- python-work/chapter5/no-users-5-9.py
+++ python-work/chapter5/no-users-5-9.py
@@ -3,3 +3,19 @@
 # a) If the list is empty, print the message We need to find some users!
 # b) Remove all of the usernames from your list, and make sure the correct
 # message is printed.
+
+# usernames = ['mansoor', 'faisal', 'salman', 'fahad', 'admin']
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            greetings_message1 = ' '.join((f"Hello {username}, would you like to",
+                                        "see a status report?"))
+            print(greetings_message1)
+        else:
+            greetings_message2 = ' '.join((f"Greetings {username}, thank you",
+                                        "for logging in again!"))
+            print(greetings_message2)
+else:
+    print('\nWe need to find some users!')
