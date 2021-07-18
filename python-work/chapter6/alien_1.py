ebf2e5e9b87e1ace0d5c264d10c8ee7c643ab88e
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 18:35:13 +1000

    modifying-key-values

--- python-work/chapter6/alien_1.py
+++ python-work/chapter6/alien_1.py
@@ -20,3 +20,22 @@ print(alien_1)
 # Alient speed key-value
 alien_1['speed'] = 'medium'
 print(alien_1)
+
+# Print Origin position of the alien
+print(f"Original position: {alien_1['x_position']}")
+
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_1['speed'] == 'slow':
+    x_increment = 1
+elif alien_1['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be a fallen alien
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_1['x_position'] = alien_1['x_position'] + x_increment
+
+print(f"New position: {alien_1['x_position']}")
