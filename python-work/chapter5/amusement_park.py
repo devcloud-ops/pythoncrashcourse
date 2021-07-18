f66bb98be4d46bccfa0d2b0a95e3669fca2d3a90
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 12:05:07 +1000

    amusement_park_rate_based_on_age

---
+++ python-work/chapter5/amusement_park.py
@@ -0,0 +1,12 @@
+# Prints result of admission based on age
+
+#age = 3
+#age = 16
+age = 30
+
+if age < 4:
+    print('\nYour admission cost is $0.')
+elif age < 18:
+    print('\nYour admission cost is $25.')
+else:
+    print('\nYour admission cost is $40.')
