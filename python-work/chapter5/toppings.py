fc2729088d6327f33c0cfd87adc8755df77005e2
DevCloudOps <devcloud.ops@outlook.com>
2021-07-18 14:13:18 +1000

    if-condition-to-check-if-list-is-not-empty

--- python-work/chapter5/toppings.py
+++ python-work/chapter5/toppings.py
@@ -1,6 +1,7 @@
 #requested_topping = 'mushrooms'
 
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+requested_toppings = []
 
 # if requested_topping != 'anchovies':
 #     print("Hold the anchovies!")
@@ -8,11 +9,13 @@ requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 # if 'mushrooms' in requested_topping:
 #     print('great job, proceed with the order!')
     
-for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("\nSorry, we are out of green peppers right now.")
-    else:
-        print(f"\nAdding {requested_topping}.")
-
-print("\nFinished making your pizza!")
-
+if requested_toppings:  # This condition checks if requested_toppings isn't 
+                        # empty
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("\nSorry, we are out of green peppers right now.")
+        else:
+            print(f"\nAdding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
