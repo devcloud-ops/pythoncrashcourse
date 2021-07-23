0fd4236245dc2b49b131515c490cebab51ae4ea3
DevCloudOps <devcloud.ops@outlook.com>
2021-07-23 15:51:22 +1000

    looping_through_users_data

---
+++ python-work/chapter6/user.py
@@ -0,0 +1,10 @@
+user_0 = {
+    'username': 'muhammadfaisal.siddiqui',
+    'first': 'muhammad faisal',
+    'last': 'siddiqui'
+}
+
+for key, value in user_0.items():
+    print(f'\nKey: {key}')
+    print(f'Value: {value.title()}')
+
