e7e2cd46c94c7dddab8ddb0dae68f0748e3ef1b9
DevCloudOps <devcloud.ops@outlook.com>
2021-07-23 20:20:56 +1000

    unique_values

--- python-work/chapter6/fav_lang_values_loop.py
+++ python-work/chapter6/fav_lang_values_loop.py
@@ -4,13 +4,25 @@ favorite_languages = {
     'sarah': 'c',
     'edward': 'ruby',
     'phil': 'java',
+    'alan': 'python'
     }
 
-for value in sorted(favorite_languages.values()):
-    print(f"{value.title()}")
+# for value in sorted(favorite_languages.values()):
+#     print(f"{value.title()}")
 
 # Output: 
 # C
 # Java  
 # Python
+# Python
 # Ruby
+
+# Get unique values:
+for value in set(favorite_languages.values()):
+    print(f"{value.title()}")
+
+# Output: 
+# C
+# Ruby
+# Java
+# Python
