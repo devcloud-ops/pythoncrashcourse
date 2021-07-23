1c056499bf97bb889002be88ba4ae457f421f91c
DevCloudOps <devcloud.ops@outlook.com>
2021-07-23 12:52:55 +1000

    testing-get-functionality

--- python-work/chapter6/alien_0_get_function.py
+++ python-work/chapter6/alien_0_get_function.py
@@ -1,4 +1,9 @@
 alien_0 = {'color': 'green', 'speed': 'slow'}
+#alien_0 = {'color2': 'green', 'speed': 'slow'}
 
 point_value = alien_0.get('points', 'No point value assigned.')
+
+point_value2 = alien_0.get('color', 'color is not specified')
+
 print(point_value)
+print(point_value2)
