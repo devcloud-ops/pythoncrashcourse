8d951f2f3a8a6bdd4eeb08d4efed622ec186fc4a
DevCloudOps <devcloud.ops@outlook.com>
2021-07-24 17:53:04 +1000

    changing_colors_aliens_first3

--- python-work/chapter6/thirty_aliens_loop_creation.py
+++ python-work/chapter6/thirty_aliens_loop_creation.py
@@ -5,9 +5,24 @@ for number_of_alien in range(30):
     number_of_alien = {'color': 'green', 'points':5, 'speed': 'slow'}
     aliens.append(number_of_alien)
 
-for alien in aliens[:5]:
-    print(alien)
-print("...")
-
 # Show how many aliens were created.
 print(f"Total number of aliens created: {len(aliens)}")
+
+print("Before change...")
+
+for alien in aliens[:5]:
+    print(alien)
+
+# Changing first 3 aliens to green:
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+print("...")
+print("After change...")
+
+
+for alien in aliens[:5]:
+    print(alien)
