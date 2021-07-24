b48ccef299a4755c9134b8eaf6c86b880f034076
DevCloudOps <devcloud.ops@outlook.com>
2021-07-24 20:08:50 +1000

    two-scenarios

--- python-work/chapter6/many_users.py
+++ python-work/chapter6/many_users.py
@@ -18,7 +18,29 @@ users = {
 #     for property,value in users_info.items():
 #         print(f'{property.title()}: {value.title()}')
 
+# Output:
+# Username: Aeinstein
+# First: Albert
+# Last: Einstein
+# Location: Princeton
+
+# Username: Mcurie
+# First: Marie
+# Last: Curie
+# Location: Paris
+
+
 for username, users_info in users.items():
     print(f'\nUsername: {username}')
     print(f"Full Name: {users_info['first'].title()} {users_info['last'].title()}")
     print(f"User's Current Location: {users_info['location'].title()}")
+
+# Output:
+
+# Username: aeinstein
+# Full Name: Albert Einstein
+# User's Current Location: Princeton
+
+# Username: mcurie
+# Full Name: Marie Curie
+# User's Current Location: Paris
