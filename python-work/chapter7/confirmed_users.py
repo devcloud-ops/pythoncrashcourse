4b997e8ab99f19e769c556aa939cdb63f407b1e3
DevCloudOps <devcloud.ops@outlook.com>
2021-07-25 18:26:40 +1000

    commented

--- python-work/chapter7/confirmed_users.py
+++ python-work/chapter7/confirmed_users.py
@@ -6,7 +6,8 @@ confirmed_users = []
 
 # Verify each user until there are no more unconfirmed users.
 # Move each verified user into the list of confirmed users.
-while unconfirmed_users:
+
+while unconfirmed_users: # Run this loop until unconfirmed_users list is empty
     current_user = unconfirmed_users.pop()
     print(f"\nVerifying user: {current_user.title()}")
     confirmed_users.append(current_user)
