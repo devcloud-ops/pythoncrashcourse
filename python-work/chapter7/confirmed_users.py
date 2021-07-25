0b95adb16483cfc529e9e638ec4b9d8979d7f09d
DevCloudOps <devcloud.ops@outlook.com>
2021-07-25 18:29:40 +1000

    commented2

--- python-work/chapter7/confirmed_users.py
+++ python-work/chapter7/confirmed_users.py
@@ -8,7 +8,13 @@ confirmed_users = []
 # Move each verified user into the list of confirmed users.
 
 while unconfirmed_users: # Run this loop until unconfirmed_users list is empty
+    
     current_user = unconfirmed_users.pop()
+    # pop() is an inbuilt function in Python that removes and returns last 
+    # value from the list or the given index value. Parameter: index (optional)
+    # The value at index is popped out and removed. If the index is not given,
+    # then the last element is popped out and removed.
+    
     print(f"\nVerifying user: {current_user.title()}")
     confirmed_users.append(current_user)
     
