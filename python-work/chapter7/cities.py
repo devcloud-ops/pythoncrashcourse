8c168468b2be69c90d48ccd6b64460dc926de76c
DevCloudOps <devcloud.ops@outlook.com>
2021-07-25 16:13:43 +1000

    comments

--- python-work/chapter7/cities.py
+++ python-work/chapter7/cities.py
@@ -3,7 +3,8 @@
 prompt = "\nPlease enter the name of a city you have visited:"
 prompt += "\n(Enter 'quit' when you are finished.) "
 
-while True:
+while True: # A loop that starts with while True will run forever unless it 
+            # reaches a break statement
     city = input(prompt)
     if city == 'quit':
         break
