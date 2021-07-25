61bc10e6f92697050a62943736594136ae04b2e9
DevCloudOps <devcloud.ops@outlook.com>
2021-07-25 18:04:13 +1000

    movie_tickets_7_5

--- python-work/chapter7/movie_tickets_7_5.py
+++ python-work/chapter7/movie_tickets_7_5.py
@@ -4,3 +4,19 @@
 # over age 12, the ticket is $15. Write a loop in which you ask users their 
 # age, and then tell them the cost of their movie ticket.
 
+prompt = "\nEnter the person age you'd like to buy tickets for. "
+prompt += "\nEnter a 'finish' when you are done!: "
+age = ''
+
+while True:
+    age = input(prompt)
+    if age == 'finish':
+        break
+    else:
+        age = int(age)
+        if age < 3:
+            print("\nThis person's ticket is free!")
+        elif age >= 3 and age <= 12:
+            print("\nThis person's ticket fare is $10")
+        elif age > 12:
+            print("\nThis person's ticket fare is $15")
