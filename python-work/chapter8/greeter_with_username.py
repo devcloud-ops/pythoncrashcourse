3a205224b46bf9b8a8660fdbdf3834ddf5e4326a
DevCloudOps <devcloud.ops@outlook.com>
2021-07-26 13:09:07 +1000

    greetuser_with_parameter

---
+++ python-work/chapter8/greeter_with_username.py
@@ -0,0 +1,10 @@
+# Functions :)
+
+def greet_user(username): # Function definition which accepts parameter 'username'
+    """Display a simple greeting.""" #docstring: what this function does!
+    print(f"Hello {username.title()}!")
+
+greet_user('mansoor') # argument with value 'mansoor' is passed to the function
+greet_user('faisal')
+greet_user('salman')
+greet_user('fahad')
