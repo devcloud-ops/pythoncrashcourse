85d3d9e9d27ccff5fb86a83480e221610bec702c
DevCloudOps <devcloud.ops@outlook.com>
2021-07-26 16:58:01 +1000

    re-organized

--- python-work/chapter8/printing_models_modify_list_inside_function.py
+++ python-work/chapter8/printing_models_modify_list_inside_function.py
@@ -1,7 +1,3 @@
-# Start with some designs that need to be printed.
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
-
 def print_models(unprinted_designs, completed_models):
     """Simulate printing each design, until none are left.
     Move each design to completed_models after printing."""
@@ -17,6 +13,11 @@ def show_completed_models(completed_models):
     print("The following models have been printed:")
     for completed_model in completed_models:
         print(completed_model)
+
+
+# Start with some designs that need to be printed.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
 
 print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
