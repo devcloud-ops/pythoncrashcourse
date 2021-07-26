744d6cc3b27456caf9325a5c1c8472d2afec251a
DevCloudOps <devcloud.ops@outlook.com>
2021-07-26 23:22:30 +1000

    class-dog

---
+++ python-work/chapter9/dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    """A simple attempt to model a dog."""
+    
+    def __init__(self,name,age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+        
+    def sit(self):
+        """Simulating a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+        
+    def roll_over(self):
+        """Simulating rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
