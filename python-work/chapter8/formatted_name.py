90c3c28d1552fdedb6e160f83c06e8dadcad1fd9
DevCloudOps <devcloud.ops@outlook.com>
2021-07-26 15:01:59 +1000

    formatted_name

---
+++ python-work/chapter8/formatted_name.py
@@ -0,0 +1,9 @@
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jiMi', 'hEnDrix')
+print(musician)
+
+print(get_formatted_name('faiSal','siDDiqui'))
