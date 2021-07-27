d67cbac5fa1d004a7577cef4111c69c6402146d5
DevCloudOps <devcloud.ops@outlook.com>
2021-07-27 21:10:21 +1000

    comment_init_method

--- python-work/chapter9/dog.py
+++ python-work/chapter9/dog.py
@@ -1,7 +1,9 @@
 class Dog:
     """A simple attempt to model a dog."""
     
-    def __init__(self,name,age):
+    def __init__(self,name,age): # This method will run automatically upon 
+        # instantiation because of its name "init" and the two leading and 
+        # trailing underscores.
         """Initialize name and age attributes."""
         self.name = name
         self.age = age
