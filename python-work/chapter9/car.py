fe7e8f21e4e6267384663e4ee5182699b0577e89
DevCloudOps <devcloud.ops@outlook.com>
2021-07-27 22:41:24 +1000

    new_car

--- python-work/chapter9/car.py
+++ python-work/chapter9/car.py
@@ -6,8 +6,14 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0 # Definition here will suffice with a default
+                                  # value
         
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        """Displays car's mileage."""
+        print(f"This car has traveled {self.odometer_reading} miles.")
--- python-work/chapter9/new_car.py
+++ python-work/chapter9/new_car.py
@@ -4,3 +4,6 @@ my_new_car = c.Car('audi', 'a4', 2019)
 
 print(my_new_car.get_descriptive_name())
 # Output: 2019 Audi A4
+
+my_new_car.read_odometer()
+# Output: This car has traveled 0 miles.
