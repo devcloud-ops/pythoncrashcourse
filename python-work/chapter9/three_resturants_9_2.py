05d6e18b350ef0ff0149761cc94970429c0a0cf6
DevCloudOps <devcloud.ops@outlook.com>
2021-07-27 21:58:10 +1000

    three-resturants-9-3

---
+++ python-work/chapter9/three_resturants_9_2.py
@@ -0,0 +1,22 @@
+# 9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three
+# different instances from the class, and call describe_restaurant() for each
+# instance.
+
+import resturant as r
+
+print("\n\n")
+
+resturant1 = r.Resturant('gulf royal chinese resturant','chinese')
+resturant1.describe_resturant()
+
+print("\n\n")
+
+resturant2 = r.Resturant('bundoo khan','pakistani')
+resturant2.describe_resturant()
+
+print("\n\n")
+
+resturant3 = r.Resturant('Romancia','Saudi')
+resturant3.describe_resturant()
+
+
